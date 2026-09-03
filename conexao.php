ddbd63cfcf63befb8c728df947a7a1dbb8b98ba4
etegravatammc <etegravatammc@gmail.com>
2026-09-03 14:12:30 +0000

    Criado arquivo OO da conexão ao banco

---
+++ conexao.php
@@ -0,0 +1,17 @@
+<?php
+class Conexao {
+    private static $instancia = null;
+
+    public static function getConexao() {
+        if (self::$instancia === null) {
+            try {
+                self::$instancia = new PDO("mysql:host=localhost;dbname=novo;charset=utf8", "denisson", "123456");
+                self::$instancia->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Erro na conexão com banco de dados: " . $e->getMessage());
+            }
+        }
+        return self::$instancia;
+    }
+}
+?>
